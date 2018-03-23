48f781cdb67f69993ab7dd6e9c9e32a7c02874c0
onlinereg <37691506+onlinereg@users.noreply.github.com>
2018-03-24 00:08:47 +0530

    Add files via upload

--- js/proxyhome.js
+++ js/proxyhome.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	
+		$('#username').text("Welcome "+ $.cookie("Username"));
+			
+		$('#deleteCookie').on("click", function(){
+			$.removeCookie("Username");
+		});
+	
 		$('#Signuppop').on("click", function(){
 			$('.popup_container').removeClass('displayNone');
 			$('.popupin_container').addClass('displayNone');
@@ -12,8 +19,13 @@ $(document).ready(function(){
 		$('.close, .cancelbtn').on("click", function(){
 			$('.popup_container').addClass('displayNone');
 			$('.popupin_container').addClass('displayNone');
+			$('.Spopup_container').addClass('displayNone');		
 		});
 	    
+		$('.Specialclose').on("click", function(){
+			$('.Spopup_container').addClass('displayNone');
+		});
+		
 		$('#signin').on("click", function(){
 	   if($('input[name="token"]').val() == "Admin")
 	   {
@@ -25,4 +37,106 @@ $(document).ready(function(){
 	   $('#Register').on("click", function(){
 			$('.gsheet_Container').removeClass('displayNone');
 		});
+		
+		$('input[type=text]').on("click", function(){
+			$(this).removeClass('errorBtn');
+		});
+		
+		$('input[type=password]').on("click", function(){
+			$(this).removeClass('errorBtn');
+		});
+		
+		//var $form = $('form#Signinmodel')
+		
+
+		$('#Signupbtnid').on('click', function(e) {
+		  e.preventDefault();
+			var email = $('input[name=email]').val();
+			var name = $('input[name=name]').val();
+		    var pin = $('input[name=pin]').val();
+			
+			var url = "https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1fJOSaqX9dSagCQJF09drAiheRiaJ9oYT43_upL0RjbY&sheet=login";
+			$.ajax({
+				url:url,
+				dataType:"jsonp",
+				success:function(data) {
+					
+					var UserData = data.login;
+					var EnterData = $('input[name=token]').val();
+
+					
+					  for(var a = 0 ; a < UserData.length ; a++ )
+					  {
+						  if(UserData[a].pin == pin)
+						  {
+							$('input[type=text]').addClass('errorBtn');
+							$('input[type=password]').addClass('errorBtn');
+							return;
+						  }
+					  }
+							
+					var data = {'email':email , 'name':name, 'pin':pin};
+					var  url = 'https://script.google.com/macros/s/AKfycbzmnBmH9Jnny2SUlNR2Bh-i7_AHn-u_NMEB32y_amG4YW8c2tt8/exec';
+					var jqxhr = $.ajax({
+					url: url,
+					method: "GET",
+					dataType: "json",
+					data: data,
+					success: function(resopnse){
+					
+						console.log("Success");
+						$('.popup_container').addClass('displayNone');
+						$('.Spopup_container').removeClass("displayNone");
+				  }
+				  });			
+				},
+			});	
+		});
+		
+		 var LoggerName;
+		$('#signin').on('click', function(e) {
+		  e.preventDefault();
+		  var url = "https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1fJOSaqX9dSagCQJF09drAiheRiaJ9oYT43_upL0RjbY&sheet=login";
+		$.ajax({
+			url:url,
+			dataType:"jsonp",
+			success:function(data) {
+			console.log(data);
+			console.log(data.login[0].pin);
+				
+			var UserData = data.login;
+			  var EnterData = $('input[name=token]').val();
+			 
+			  for(var a = 0 ; a < UserData.length ; a++ )
+			  {
+				  if(UserData[a].pin == EnterData)
+				  {
+					  console.log("Match Found "+UserData[a].name);
+					  LoggerName = UserData[a].name;
+					  $.cookie("Username", LoggerName);
+					  window.location.href = "./description.html";
+					  
+				  }
+				  else
+				  {
+						$('input[type=text]').addClass('errorBtn');
+						$('input[type=password]').addClass('errorBtn');
+				  }
+			  }
+			  
+			//  $('#username').text("Welcome "+LoggerName+",");
+			}
+		});
+			 	  	  
+	});
+		
+		/*var url = "https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1fJOSaqX9dSagCQJF09drAiheRiaJ9oYT43_upL0RjbY&sheet=login";
+		$.ajax({
+			url:url,
+			dataType:"jsonp",
+			success:function(data) {
+				console.log(data);
+				console.log(data.login[0].pin);
+			},
+		});*/
 });
